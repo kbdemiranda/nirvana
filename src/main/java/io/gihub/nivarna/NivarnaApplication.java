0765636c3a561cb3914b3579f70dbc7f5f2ff630
Kaique de Miranda <kbdemiranda@hotmail.com>
2023-10-01 15:11:06 -0300

    Iniciando o desenvolvimento

---
+++ src/main/java/io/gihub/nivarna/NivarnaApplication.java
@@ -0,0 +1,13 @@
+package io.gihub.nivarna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NivarnaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NivarnaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/gihub/nivarna/NivarnaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gihub.nivarna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NivarnaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
